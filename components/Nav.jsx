43fe5b3ba9abeb72eadc38ecf7f246d1d1ce37fc
AKSHAYBINDU <akshaykumarkanad@gmail.com>
2024-08-15 20:12:35 +0530

    "implement navbar components"

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,10 +1,26 @@
+"use client";
 import Image from "next/image";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 import Link from "next/link";
-// import { useState } from "react/cjs/react.production.min";
+// import logo from "../public/assets/images/logo.svg";
+import { useEffect, useState } from "react";
 
 const Nav = () => {
-  const isUserLoggedIn = true;
+  // const isUserLoggedIn = true;
+
+  const [providers, setProviders] = useState(null);
+
+  const [toggleDropdown, settoggleDropdown] = useState(false);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+
+      setProviders(response);
+    };
+    setProviders();
+  }, []);
+
   return (
     <nav className=" flex justify-between w-full mb-16 pt-3">
       <Link href="/" className=" flex gap-2 justify-center">
@@ -24,10 +40,90 @@ const Nav = () => {
             <Link href="/create-prompt" className=" black_btn">
               Create Post
             </Link>
-            <button type="button"></button>
+            <button type="button" onClick={signOut} className="outline-btn">
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                width={30}
+                height={30}
+                className=" rounded-full"
+                alt="profile"
+              />
+            </Link>
           </div>
         ) : (
-          <div></div>
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className=" black-btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
+        )}
+      </div>
+
+      <div className=" sm:hidden flex relative">
+        {isUserLoggedIn ? (
+          <div className=" flex">
+            <Image
+              src="/assets/images/logo.svg"
+              width={30}
+              height={30}
+              className=" rounded-full"
+              alt="profile"
+              onClick={() => settoggleDropdown((prev) => !prev)}
+            />
+            {toggleDropdown && (
+              <div className=" dropdown">
+                <Link
+                  href="/profile"
+                  className=" dropdown_link"
+                  onClick={() => settoggleDropdown(false)}
+                >
+                  My Proflie
+                </Link>
+                <Link
+                  href="/create"
+                  className=" dropdown_link"
+                  onClick={() => settoggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type="button"
+                  onClick={() => {
+                    settoggleDropdown(false);
+                    signOut();
+                  }}
+                  className=" black-btn mt-5 w-full black_btn"
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <button
+                  type="button"
+                  key={provider.name}
+                  onClick={() => signIn(provider.id)}
+                  className=" black-btn"
+                >
+                  Sign In
+                </button>
+              ))}
+          </>
         )}
       </div>
     </nav>
